ba3862e8d4ccd10aebae9dcf0a6724685ea85e87
phamtienanh01 <Phamtienanh0@gmail.com>
2024-01-25 22:04:49 +0700

    fix table

--- src/components/table/TableBootstrap.jsx
+++ src/components/table/TableBootstrap.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { Table } from "react-bootstrap"
-import "bootstrap/dist/css/bootstrap.min.css"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Table } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import moment from 'moment';
 
 export const TableBootstrap = () => {
-
   const [data, setData] = useState([]);
 
   const getData = async () => {
     try {
-      const response = await axios.get('https://ap-southeast-1.aws.data.mongodb-api.com/app/application-0-rxqgf/endpoint/get_table_data');
-      // setData(response.data);
-      setData(response.data.slice(0, 10));
-      
-      // console.log("data", response.data[0]);
+      const response = await axios.get('https://ap-southeast-1.aws.data.mongodb-api.com/app/application-0-rxqgf/endpoint/get_table_data', {
+        params: {
+          sort: { time: -1 }, // Sắp xếp theo trường 'time', từ lớn đến bé (mới nhất đến cũ nhất)
+          limit: 10 // Giới hạn chỉ lấy 10 dòng dữ liệu
+        }
+      });
+      setData(response.data.reverse()); // Đảo ngược mảng dữ liệu mới nhất lên trên cùng
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -23,11 +23,11 @@ export const TableBootstrap = () => {
 
   useEffect(() => {
     getData();
-  }, [data]);
+  }, []);
 
   const formatTime = (time) => {
     return moment(time).format('YYYY-MM-DD HH:mm:ss');
-};
+  };
 
   return (
     <Table className='table table-striped'>
@@ -43,13 +43,12 @@ export const TableBootstrap = () => {
         {data.map((item, index) => (
           <tr key={index}>
             <th>{index + 1}</th>
-            <td style={{width:'400px'}}>{formatTime(item.time)}</td>
-            <td >{item.currentangle}</td>
-            <td style={{width:'300px'}}>{item.prediction}</td>
+            <td style={{ width: '400px' }}>{formatTime(item.time)}</td>
+            <td>{item.currentangle}</td>
+            <td style={{ width: '300px' }}>{item.prediction}</td>
           </tr>
         ))}
       </tbody>
     </Table>
-    
-  )
-}
+  );
+};
